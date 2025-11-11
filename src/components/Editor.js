de1fd10570c1443c8e3db90da02cdd8578bcf0d5
SettyBhavithav <settybunny2006@gmail.com>
2025-11-11 16:22:39 +0530

    Added code auto-formatting feature triggered by Shift+Alt+F key combination

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -36,3 +36,5 @@
 // Captured standard output and error streams from code execution process
 
 // Keyboard shortcuts configured for file saving and code execution
+
+// Added code auto-formatting feature triggered by Shift+Alt+F key combination
